292d412d487f9ae03a931c032ffc8c04e6851f22
Enrico Goerlitz <private.email@gmail.com>
2024-06-20 16:55:13 +0200

    u

--- exercises/07-data-sources/fetch.tf
+++ exercises/07-data-sources/fetch.tf
@@ -1,3 +1,16 @@
+data "aws_ami" "aws_linux" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 data "aws_caller_identity" "current" { }
 
 data "aws_region" "current" { }
@@ -22,4 +35,8 @@ output "aws_region" {
 
 output "aws_vpc" {
   value = data.aws_vpc.main
+}
+
+output "aws_linux_ami" {
+  value = data.aws_ami.aws_linux
 }
